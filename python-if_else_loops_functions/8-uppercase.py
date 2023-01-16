40cae731730d241165bf43c21241829e605e4578
Repli1 <5724@holbertonstudents.com>
2023-01-16 04:23:03 -0800

    Add task 8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def uppercase(str):
+    n = 0
+    for i in range (0, len(str)):
+        if 96 < ord(str[i]) < 122:
+           n = 32
+        else:
+            n = 0
+        if i == (len(str) - 1):
+            print(chr((ord(str[i]) - n)).format())
+        else:
+            print(chr((ord(str[i]) - n)).format(), end="")
