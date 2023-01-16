0447d714d977b96630d304d2ad260eff28e92604
Repli1 <5724@holbertonstudents.com>
2023-01-16 08:34:35 -0800

    fix3

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+__import__("0-add")
 if __name__ == "__main__":
     from add_0 import add
     a = 1
     b = 2
-    print(f"{a} + {b} = {add(1, 2)}".format())
+    print(f"{a} + {b} = {add(a, b)}".format())
