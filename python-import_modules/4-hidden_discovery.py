3155abba2802bd3c9fc6733ec317cf949034cfe4
Repli1 <5724@holbertonstudents.com>
2023-01-17 05:23:19 -0800

    fix3

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,6 +2,6 @@
 if __name__ == "__main__":
     import hidden_4
     names = dir(hidden_4)
-    for i in names:
+    for i in range(0, len(names)):
         if names[i][0] != '_' and names[i][1] != '_':
             print(names[i])
