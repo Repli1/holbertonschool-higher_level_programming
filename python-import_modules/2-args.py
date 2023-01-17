51fe4acefa5e1e9a5616a3500e4e24dce9b21359
Repli1 <5724@holbertonstudents.com>
2023-01-16 22:41:48 -0800

    Fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import sys
-if (len(sys.argv) - 1) == 1:
-    print("1 argument:")
-elif (len(sys.argv) - 1) == 0:
-    print("0 arguments.")
-else:
-    print(f"{len(sys.argv) - 1} arguments:")
-for i in range(1, len(sys.argv)):
+if __name__ == "__main__":
+    if (len(sys.argv) - 1) == 1:
+        print("1 argument:")
+    elif (len(sys.argv) - 1) == 0:
+        print("0 arguments.")
+    else:
+        print(f"{len(sys.argv) - 1} arguments:")
+    for i in range(1, len(sys.argv)):
         print(f"{i}: {sys.argv[i]}")
