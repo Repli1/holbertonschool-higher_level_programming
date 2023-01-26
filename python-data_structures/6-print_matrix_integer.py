196cf361c958967823f8cc38cac69726a8474bb7
Repli1 <cladescandor42@gmail.com>
2023-01-26 06:23:10 -0800

    fix empty matrix edgecase

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix is None:
+    if len(matrix) == 0:
         print("")
         return None
     for i in range(0, len(matrix)):
