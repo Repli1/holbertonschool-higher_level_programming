d9b79f68f0502a0904a3b2e260db87f7ffedb2b9
Repli1 <cladescandor42@gmail.com>
2023-01-27 07:47:05 -0800

    Add task 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if len(a_dictionary) == 0:
+        return 0
     for key in sorted(a_dictionary):
         print(f"{key}: {a_dictionary[key]}")
