bb22c14ebe929970261e9ec2bdff5eae25e46ee2
Repli1 <cladescandor42@gmail.com>
2023-01-29 15:55:14 -0800

    PEP8

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,7 +7,7 @@ def safe_print_division(a, b):
     except:
         return None
     finally:
-        if c == None:
+        if c is None:
             print("Inside result: None")
         else:
             print("Inside result: {:0.1f}".format(c))
