edff12c0a5ea157cc219e478555c3cf39f8e8618
Repli1 <cladescandor42@gmail.com>
2023-01-29 15:53:45 -0800

    Add task 3

---
+++ python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+def safe_print_division(a, b):
+    try:
+        c = None
+        c = a / b
+        return c
+    except:
+        return None
+    finally:
+        if c == None:
+            print("Inside result: None")
+        else:
+            print("Inside result: {:0.1f}".format(c))
