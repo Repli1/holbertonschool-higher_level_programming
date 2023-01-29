3555a4d72a8e351109739405827d571b243363d7
Repli1 <cladescandor42@gmail.com>
2023-01-28 22:45:45 -0800

    Add task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    translate = {"I" : 1, "V" : 5, "X" : 10, "L" : 50, "C" : 100, "D" : 500, "M" : 1000}
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    n = 0
+    for i in range(0, len(roman_string)):
+        if i != len(roman_string) - 1:
+            if translate[roman_string[i]] < translate[roman_string[i + 1]]:
+                n -= translate[roman_string[i]]
+            else:
+                n += translate[roman_string[i]]
+        else:
+            n += translate[roman_string[i]]
+    return n
