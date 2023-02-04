6bb2d075559d3f77a59b9eebbc578a4e2556a454
Repli1 <cladescandor42@gmail.com>
2023-02-04 01:36:08 -0800

    Add task 1

---
+++ python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines a class named Square"""
+
+class Square():
+    """Defines a private attribute named size"""
+    def __init__(self, size):
+        self._Square__size = size
