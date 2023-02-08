ca106de152f1a0bbd32be4a25aed9a5be1c0c9d9
Repli1 <cladescandor42@gmail.com>
2023-02-08 05:21:58 -0800

    fix

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,16 +5,12 @@
 class Square():
     """Defines a private attribute named size"""
     def __init__(self, size=0, position=(0, 0)):
-        self._Square__size = size
-        self._Square__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
         return self._Square__size
-
-    @property
-    def position(self):
-        return self._Square__position
 
     @size.setter
     def size(self, value):
@@ -26,13 +22,18 @@ class Square():
             raise ValueError
         self._Square__size = value
 
+    @property
+    def position(self):
+        return self._Square__position
+
     @position.setter
     def position(self, array):
-        if type(array) is tuple and len(array) == 2:
-            pass
-        else:
-            print("position must be a tuple of 2 positive integers")
-            raise TypeError
+        if type(array) is not tuple or len(array) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(array[0]) is not int or type(array[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if array[0] < 0 or array [1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
         self._Square__position = array
 
     def area(self):
@@ -42,7 +43,7 @@ class Square():
         for l in range(0, self._Square__position[1]):
             print("")
         for j in range(0, self._Square__size):
-            for k in range (0, self._Square__position[0]):
+            for k in range(0, self._Square__position[0]):
                 print(" ", end="")
             for i in range(1, self._Square__size):
                 print("#", end="")
