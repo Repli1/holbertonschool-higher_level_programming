188e985e98a5138b9e6c0e7e0e9203624d3587b8
Repli1 <cladescandor42@gmail.com>
2023-02-08 10:55:55 -0800

    PEP8

--- python-more_classes/5-rectangle.py
+++ python-more_classes/5-rectangle.py
@@ -50,6 +50,7 @@ class Rectangle:
 
     def __repr__(self):
         return f"Rectangle({self.__width}, {self.__height})"
+
     def __del__(self):
         del self
         print("Bye rectangle...")
