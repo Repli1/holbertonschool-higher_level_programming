92ea7d506d148b19b159ac3f0a8f94a531207aba
Repli1 <cladescandor42@gmail.com>
2023-02-08 04:18:32 -0800

    Add task 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Defines a class named Square"""
+
+
+class Square():
+    """Defines a private attribute named size"""
+    def __init__(self, size=0):
+        self._Square__size = size
+
+    @property
+    def size(self):
+        return self._Square__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            print("size must be an integer", end="")
+            raise TypeError
+        if value < 0:
+            print("size must be >= 0", end="")
+            raise ValueError
+        self._Square__size = value
+
+    def area(self):
+        return self._Square__size**2
+    def my_print(self):
+        for j in range (0, self._Square__size):
+            for i in range(1, self._Square__size):
+                print("#", end="")
+            print("#")
+        if self._Square__size == 0:
+            print("")
