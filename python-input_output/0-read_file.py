90c386e791d6beac882b6871e80039ea898565ce
Repli1 <cladescandor42@gmail.com>
2023-02-10 12:23:18 -0800

    Add documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Function read_file"""
+
 
 def read_file(filename=""):
+    """Prints a file to the standard output"""
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
     print(read_data)
