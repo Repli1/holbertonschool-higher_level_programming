658663b01eb413639e09134827d5a08878303e62
Repli1 <cladescandor42@gmail.com>
2023-02-10 07:28:58 -0800

    Add documentation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""A class that inherits from the list class"""
 
 class MyList(list):
-    """"""
+    """A class that inherits from the list class"""
     def print_sorted(self):
         print(sorted(self))
     
