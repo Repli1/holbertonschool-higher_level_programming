d919027b5f9e0e32b7a49613dadbf12ee0e052e8
Repli1 <cladescandor42@gmail.com>
2023-02-10 09:42:30 -0800

    PEP8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -2,6 +2,7 @@
 """Rectangle class"""
 BaseGeometry = __import__("7-base_geometry").BaseGeometry
 
+
 class Rectangle(BaseGeometry):
     """Rectangle class"""
     def __init__(self, width, height):
