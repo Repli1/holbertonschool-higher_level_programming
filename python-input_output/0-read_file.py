37b3542461c2005be14cdf0139c7169d238c7aba
Repli1 <cladescandor42@gmail.com>
2023-02-10 12:24:09 -0800

    Removed newline

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Prints a file to the standard output"""
     with open(filename, "r", encoding="utf-8") as f:
         read_data = f.read()
-    print(read_data)
+    print(read_data, end="")
