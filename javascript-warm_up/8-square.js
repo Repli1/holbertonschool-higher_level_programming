e25e8aff06a2bc7a981fb6c5bd08f2d882744472
Repli1 <cladescandor42@gmail.com>
2023-04-10 23:56:18 -0700

    Correct error message

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 const process = require('process');
 const args = process.argv;
 if (isNaN(args[2]) === true) {
-  console.log('Missing number of occurrences');
+  console.log('Missing size');
 } else {
   for (let i = 0; i < parseInt(args[2]); i++) {
     console.log('X'.repeat(parseInt(args[2])));
