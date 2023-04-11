6424356a40bb52eff75543c1ab45e797afd891ce
Repli1 <cladescandor42@gmail.com>
2023-04-11 00:29:52 -0700

    Fix semistandard

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -6,7 +6,7 @@ if (isNaN(args[2]) === true) {
 } else {
   function factorial (a) {
     if (a === 1) {
-      return (1)
+      return (1);
     } else {
       return (a * factorial(a - 1));
     }
