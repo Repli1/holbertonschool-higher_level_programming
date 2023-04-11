6bfd91dba913bc6ae6af326b69012254e41333d2
Repli1 <cladescandor42@gmail.com>
2023-04-11 00:04:54 -0700

    Fix semistandard

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -4,8 +4,8 @@ const args = process.argv;
 if (isNaN(args[2]) === true || isNaN(args[3]) === true) {
   console.log('NaN');
 } else {
-  function add(a, b) {
-	  return a + b
+  function add (a, b) {
+    return (parseInt(a) + parseInt(b));
   }
-  console.log(add(args[2], args[3]))
+  console.log(add(args[2], args[3]));
 }
