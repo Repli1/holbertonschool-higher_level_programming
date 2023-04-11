a91ce4146eb416a1307efd61d7aafc30a447dc00
Repli1 <cladescandor42@gmail.com>
2023-04-11 00:27:10 -0700

    Add task 10

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+if (isNaN(args[2]) === true) {
+  console.log(1);
+} else {
+  function factorial (a) {
+    if (a === 1) {
+      return (1)
+    } else {
+      return (a * factorial(a - 1));
+    }
+  }
+  console.log(factorial(parseInt(args[2])));
+}
