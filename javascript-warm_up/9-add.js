bd47a5b08be87fdff81d973b7dc805e3e22454a8
Repli1 <cladescandor42@gmail.com>
2023-04-11 00:02:03 -0700

    Add task 9

---
+++ javascript-warm_up/9-add.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const process = require('process');
+const args = process.argv;
+if (isNaN(args[2]) === true || isNaN(args[3]) === true) {
+  console.log('NaN');
+} else {
+  function add(a, b) {
+	  return a + b
+  }
+  console.log(add(args[2], args[3]))
+}
