fd0f93e60f5b825f28a3e8b99de0fa5aaa5f15a3
Repli1 <cladescandor42@gmail.com>
2023-04-14 13:07:40 -0700

    Fix output and edgecase

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,14 +4,7 @@ const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
 } else {
-  let biggest = parseInt(args[2]);
-  let second = parseInt(args[3]);
-  for (let i = 3; i < args.length; i++) {
-    if (parseInt(args[i]) >= biggest) {
-      second = biggest;
-      biggest = parseInt(args[i]);
-    }
-  }
-  console.log(second);
-  console.log(biggest);
+  let sorted = args.slice(2, args.length + 1);
+  sorted.sort();
+  console.log(sorted[sorted.length - 2]);
 }
