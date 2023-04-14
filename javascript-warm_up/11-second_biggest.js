1e2b9e63315e551492232656b24466de79735a70
Repli1 <cladescandor42@gmail.com>
2023-04-14 13:28:22 -0700

    Fix

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,7 +4,9 @@ const args = process.argv;
 if (args.length <= 3) {
   console.log(0);
 } else {
-  let sorted = args.slice(2, args.length + 1);
-  sorted.sort();
-  console.log(sorted[sorted.length - 2]);
+  let args2 = args.slice(2, args.length + 1);
+  args2.sort(function(a, b) {
+    return a - b;
+  });
+  console.log(args2[args2.length - 2])
 }
