e3e17facc8fd2b9fc7192f537ff94060c6768eaf
Repli1 <cladescandor42@gmail.com>
2023-04-15 07:18:38 -0700

    Fix functions to work as intended

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -15,15 +15,11 @@ class Rectangle {
   }
 
   rotate () {
-    for (let i = 0; i < this.width; i++) {
-      console.log('X'.repeat(this.height));
-    }
+    this.width = [this.height, this.height = this.width][0];
   }
 
   double () {
-    for (let i = 0; i < (this.height * 2); i++) {
-      console.log('X'.repeat(this.width * 2));
-    }
+    this.width = this.width * 2, this.height = this.height * 2;
   }
 }
 
