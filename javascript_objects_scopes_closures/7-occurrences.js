b607b1838a9fce6df14f46fb98581c63941ea6dd
Repli1 <cladescandor42@gmail.com>
2023-04-15 07:46:18 -0700

    Make code semistandard-compliant

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let j = 0;
-  for (i = 0; i < list.length; i++) {
+  for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
       j++;
     }
   }
   return j;
-}
+};
