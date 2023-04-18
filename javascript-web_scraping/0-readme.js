256affa0df644cb0b700236ab1b870cc51f364f5
Repli1 <cladescandor42@gmail.com>
2023-04-18 13:57:02 +0000

    Make code semistandard-compliant

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const fs = require('fs');
 const args = process.argv;
-fs.readFile(args[2], 'utf-8', function (err, data){
+fs.readFile(args[2], 'utf-8', function (err, data) {
   if (data === undefined) {
     console.log(err);
   } else {
