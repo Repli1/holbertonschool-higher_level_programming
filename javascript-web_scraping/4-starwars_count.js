77a98e7393938a1677fa9f8c69169a3efc1842c2
Repli1 <cladescandor42@gmail.com>
2023-04-19 19:55:07 +0000

    Add task 4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const request = require('request');
+const args = process.argv;
+const url = args[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const result = JSON.parse(body);
+    const wedge = 'https://swapi-api.hbtn.io/api/people/18/';
+    let count = 0;
+    for (let i = 0; i < result.count; i++) {
+      if (result.results[i].characters.includes(wedge)) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
